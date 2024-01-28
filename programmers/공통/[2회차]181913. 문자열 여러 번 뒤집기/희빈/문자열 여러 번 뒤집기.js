82b74d661a64ed75c7217a1f10033dc4041b3562
DESKTOP-7QTI9HV\knsol <knsol1992@naver.com>
2024-01-28 23:20:22 +0900

    브랜치 리베이스

--- programmers/공통/[2회차]181913. 문자열 여러 번 뒤집기/희빈/문자열 여러 번 뒤집기.js
+++
@@ -1,9 +0,0 @@
-function solution(text, queries) {
-  let tempArr = [...text]
-  queries.map(([start, end]) => {
-    const reverseArr = tempArr.slice(start, end+1).reverse()
-    tempArr.splice(start, reverseArr.length, ...reverseArr)
-  })
-
-  return tempArr.join('')
-}
--- programmers/희빈/121221. 추억 점수/추억 점수.js
+++
@@ -1,8 +0,0 @@
-function solution(name, yearning, photo) {
-  const point = name.reduce((newObj, obj, index) => {
-    newObj[obj] = yearning[index]
-    return newObj
-  }, {})
-
-  return photo.map(list => list.reduce((acc, cur) => acc + (point[cur] || 0), 0))
-}
