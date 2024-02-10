977bb1c22c95fb36cf6dd947b7d3098902db5b87
DESKTOP-7QTI9HV\knsol <knsol1992@naver.com>
2024-02-10 10:54:17 +0900

    [5회차][나큰솔][자유 문제][알고리즘 수업 - 퀵 정렬 3 풀이]

--- baekjoon/공통/[5회차] 24091. 알고리즘 수업 - 퀵 정렬 2/큰솔/알고리즘 수업 - 퀵 정렬 2.java
+++ baekjoon/공통/[5회차] 24091. 알고리즘 수업 - 퀵 정렬 2/큰솔/알고리즘 수업 - 퀵 정렬 2.java
@@ -20,28 +20,28 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n ; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        quickSort2(arr, 0, n - 1);
+        quickSort(arr, 0, n - 1);
 
         if(k > 0) System.out.println(-1);
 
         else {
 
             for (int arr2 : arr)
-                System.out.print(arr2 +" ");
+                System.out.print(arr2 + " ");
         }
     }
 
-    private static void quickSort2(int[] arr, int left, int right) {
+    private static void quickSort(int[] arr, int left, int right) {
 
         if (left >= right) return;
 
-        int pivot = partition2(arr, left, right);
+        int pivot = partition(arr, left, right);
 
-        quickSort2(arr, left, pivot - 1);
-        quickSort2(arr, pivot + 1, right);
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
     }
 
-    private static int partition2(int[] arr, int left, int right) {
+    private static int partition(int[] arr, int left, int right) {
         int pivot = arr[right];
         int i = left - 1;
 
@@ -49,17 +49,17 @@ public class Main {
 
             if (arr[j] <= pivot) {
                 ++i;
-                swap2(arr, i, j);
+                swap(arr, i, j);
             }
         }
 
-        if(i + 1 != right) swap2(arr, i + 1, right);
+        if(i + 1 != right) swap(arr, i + 1, right);
 
         return i + 1;
 
     }
 
-    private static void swap2(int[] arr, int i, int j) {
+    private static void swap(int[] arr, int i, int j) {
 
         if(k == 0) {
 
